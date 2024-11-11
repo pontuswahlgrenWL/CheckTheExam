f0b2207d39f804e57443f4c432ebf8632d213582
Pontus Wahlgren <pontus.wahlgren@winlas.se>
2024-11-11 15:32:53 +0100

    Pass: One wrong answer should return zero

---
+++ CheckTheExam/Kata.cs
@@ -0,0 +1,7 @@
+namespace checktheexam;
+public class Kata {
+    public static int CheckExam(string[] arr1, string[] arr2)
+    {
+        return 0;
+    }
+}
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,10 +1,13 @@
+using System.Reflection;
+using checktheexam;
 namespace Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Fact (DisplayName = "Elev skickar in endast ett svar som är felaktigt. Returnerar 0.")]
+    public void OneWrongAnswer_ShouldReturnZero()
     {
-
+        var result = Kata.CheckExam(["a"], ["b"]);
+        Assert.Equal(0, result);
     }
 }
